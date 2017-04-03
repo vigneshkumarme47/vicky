5d876dea43b45ee6e35ab90b864f70eb7e20926a
vigneshkumarme47 <vigneshkumarme47@gmail.com>
2017-04-03 21:33:23 +0530

    Create Digit in tenth position.c

---
+++ Digit in tenth position.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,s;
+    scanf("%d",&a);
+    for(i=0;i<=1;i++)
+    {
+        s=a%10;
+        a=a/10;
+    }
+    printf("%d",a);
+    
+
+}
